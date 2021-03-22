39e252ed3980e903160d6149f0c8abd1e1402bf3
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-03-22 18:59:08 +0900

    All finishedgit add .

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-var canvas = BODY.add("canvas");
-
 var PANEL = 0;
-var SPHERE = 1;//names to use in place of string
+var BALL = 1;//names to use in place of string
 //optimization :D
 
 var Field = function(){
@@ -13,7 +11,7 @@ var Field = function(){
         balls.push([x,y,z,r,color,light]);
     };
     this.addPanel = function(A,B,C,D,color,light){
-        planes.push([A,B,C,D,color,light]);
+        panels.push([A,B,C,D,color,light]);
         var n;//n for normal
         //calculate the normal vector
         if(A === 0){//special case
@@ -26,6 +24,7 @@ var Field = function(){
             var n = [1,B/A,C/A];
         }
         panelNormals.push(n);
+        console.log(n);
     }
     
     this.getClosestCollision = function(a,b,c,d,e,f){//abc start def vector
@@ -37,13 +36,14 @@ var Field = function(){
             if(k < 0)continue;
             if(k < ks){
                 cInfo = [PANEL,i];
+                ks = k;
             }
         }
         for(var i = 0; i < balls.length; i++){
-            var [X,Y,Z,R,color,light] = panels[i];
+            var [X,Y,Z,R,color,light] = balls[i];
             var A = d*d+e*e+f*f;
             var B = d*a-d*X+e*b-e*Y+f*c-f*Z;
-            var C = a*a+X*X-2*a*x+b*b+Y*Y-2*b*Y+c*c+Z*Z-2*c*Z;
+            var C = a*a+X*X-2*a*X+b*b+Y*Y-2*b*Y+c*c+Z*Z-2*c*Z-R*R;
             var det = B*B-A*C;
             if(det<0){
                 continue;
@@ -57,29 +57,29 @@ var Field = function(){
                 var k = k1>k2?k2:k1;
                 if(k < 0)continue;
                 if(k < ks){
-                    cInfo = [SPHERE,i];
+                    cInfo = [BALL,i];
+                    ks = k;
                 }
             }
         }
-        
-        if(!cInfo){
+        if(cInfo === null){
             return null;
         }
         var obj;
-        var x = a+k*d;
-        var y = b+k*e;
-        var z = c+k*f;
+        var x = a+ks*d;
+        var y = b+ks*e;
+        var z = c+ks*f;
         if(cInfo[0] === PANEL){//panel
-            var normal = panelNormals[i];
+            var normal = panelNormals[cInfo[1]];
             //return the new ray
             obj = panels[cInfo[1]];
-        }else{//sphere
-            obj = panels[cInfo[1]];
+        }else{//ball
+            obj = balls[cInfo[1]];
             var normal = [x-obj[0],y-obj[1],z-obj[2]];
         }
         var projn = proj3(normal,[d,e,f]);
         
-        return [[x,y,z,d-projn[0]*2,d-projn[1]*2,d-projn[2]*2],obj];
+        return [[x,y,z,d-projn[0]*2,e-projn[1]*2,f-projn[2]*2],obj];
     };
 }
 
@@ -154,10 +154,30 @@ var vecMat3 = function(vec,mat){//multiply to get the final shape
 };
 
 
-var rayTrace = function(w,h){
-    var r = (w<h?w:h)/2;
+
+
+
+
+
+// ok from here on comes the main part of the code
+var canvas = document.getElementById("canvas");
+var width = 500;
+var height = 300;
+canvas.width = width;
+canvas.height = height;
+var ctx = canvas.getContext("2d");
+var imgdata = ctx.getImageData(0,0,width,height);
+var data = imgdata.data;
+var field = new Field();
+
+
+var rayTrace = function(){
+    var w = width;
+    var h = height;
+    var r = (w<h?w:h)*2;
+    
     for(var y = 0; y < h; y++){
-        for(var x = 0; x < h; x++){
+        for(var x = 0; x < w; x++){
             var i = (y*w+x)*4;
             var a = 0;
             var b = 0;
@@ -167,18 +187,37 @@ var rayTrace = function(w,h){
             var f = 1;
             //now transform this ray
             //transform abc
-            [a,b,c] = vecMat([a,b,c],camera);
+            [a,b,c] = vecMat([a,b,c,1],camera);
             [d,e,f] = vecMat3([d,e,f],camera);//now the ray is transformed
             var col = getColor(a,b,c,d,e,f,0);
+            var red = Math.floor(255*col[0]);
+            var gre = Math.floor(255*col[1]);
+            var blu = Math.floor(255*col[2]);
+            data[i+0] = red;
+            data[i+1] = gre;
+            data[i+2] = blu;
+            data[i+3] = 255;
         }
     }
+    ctx.putImageData(imgdata,0,0);
 };
 
-var getColor = function(a,b,c,d,e,f,dpeth){
-    if(depth > 5){
+var aaa = 0;
+
+var getColor = function(a,b,c,d,e,f,depth){
+    if(depth > 15){
         return [1,1,1];//tracking give up. returning a light source
     }else{
-        var collision =  Field.getClosestCollision(a,b,c,d,e,f);
+        var collision =  field.getClosestCollision(a,b,c,d,e,f);
+        /*//test code
+        if(collision){//only the depth
+            var [a1,b1,c1,d1,e1,f1] = collision[0];
+            var d = Math.sqrt(a1*a1+b1*b1+c1*c1);
+            var b = 1/d*7
+            return [b,b,b];
+        }
+        return [1,1,1];
+        //end test code*/
         if(!collision){//no collision, free ray, back tracking
             return [1,1,1];
         }else{//if collision
@@ -187,6 +226,7 @@ var getColor = function(a,b,c,d,e,f,dpeth){
             if(obj[5]){//5 is for the light
                 return obj[5];
             }
+            aaa++;
             var [a1,b1,c1,d1,e1,f1] = collision[0];
             var color = getColor(a1,b1,c1,d1,e1,f1,depth+1);
             //then finally do the color adjustment
@@ -195,3 +235,13 @@ var getColor = function(a,b,c,d,e,f,dpeth){
         }
     }
 };
+
+//field.addBall(0,0,10,3,[1,0.5,0.2],false);
+field.addBall(0,0,11,3,[0.8,0.8,0.8],false);
+field.addBall(10,0,10,3,[0.5,1,0.2],false);
+field.addBall(-6,-3,7,1,[1,0,0],false);
+field.addBall(0,170,140,210,[0.5,0.2,1],false);
+field.addPanel(0,0,1,13,[0.8,0.8,0.8],false);
+
+rayTrace();
+console.log(aaa);
